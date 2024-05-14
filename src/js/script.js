2d240c8bbd29f723fd71d10a15dd613194ab5545
Andrei Alexandru <97725399+andreiixe@users.noreply.github.com>
2024-05-14 21:16:14 +0300

    14 May Update
    I added a prevention measure to access the modern main page for iOS devices running versions 5.x - 9.x, by implementing a script that detects the operating system version to avoid confusion for older iOS devices.

--- src/js/script.js
+++ src/js/script.js
@@ -22,4 +22,26 @@ window.onload = function() {
     iframe.setAttribute('allowfullscreen', '');
 
     document.getElementById('videoContainer').appendChild(iframe);
+
+    function detectiOSVersion() {
+        var ua = navigator.userAgent;
+        if (/iP(hone|od|ad)/.test(navigator.platform)) {
+            var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+            var version = [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+            return version;
+        }
+        return null;
+    }
+
+    function redirectBasedOniOSVersion() {
+        var version = detectiOSVersion();
+        if (version && (version[0] === 5 || version[0] === 6 || version[0] === 7 || version[0] === 8 || version[0] === 9)) {
+            // Redirect to another page
+            window.location.href = "../../IOS/Web/index.html";
+        }
+    }
+
+    // Call the redirect function when the page loads
+    redirectBasedOniOSVersion();
 };
+
