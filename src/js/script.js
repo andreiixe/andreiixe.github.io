3716953d297eb4cdd771b97ed12586722e2b68fa
Andrei Alexandru <97725399+andreiixe@users.noreply.github.com>
2024-06-14 11:53:09 +0300

    14 June 2024 Update

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,9 @@
 window.onload = function() {
     var videos = [
-        "https://www.youtube.com/embed/HHa-blI30D0",
-        "https://www.youtube.com/embed/9SR_QJvQ74E",
-        "https://www.youtube.com/embed/hOChJNi6MgI",
-        "https://www.youtube.com/embed/B2Vmd2yRH34",
-        "https://www.youtube.com/embed/VcLywBMHZxQ",
-        "https://www.youtube.com/embed/zuwFO70RzU8",
-        "https://www.youtube.com/embed/A30dc1fNt20",
+        "https://www.youtube.com/embed/VVLAnjtpXeM",
+        "https://www.youtube.com/embed/6v5AW6FMXig",
+        "https://www.youtube.com/embed/uZjIkMT-bZk",
+        "https://www.youtube.com/embed/oQ8PI-YNGMM"
     ];
 
     var randomIndex = Math.floor(Math.random() * videos.length);
